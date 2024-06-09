4f3b7cffe16bc51e4b7ccbbc15650aefd4e5a3ba
iftakhar <iftakharalamshuvo11@gmail.com>
2024-06-09 10:29:03 +0600

    categories details added

--- src/pages/CategoryDetails/CategoryDetails.jsx
+++ src/pages/CategoryDetails/CategoryDetails.jsx
@@ -1,10 +1,14 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import Title from "../Home/Home/Title/Title";
 import Helmeta from "../../components/Helmet/Helmeta";
 import { FaShoppingCart } from "react-icons/fa";
 
 import CatModal from "./CatModal";
 import { FaEmpire } from "react-icons/fa6";
+import UseCart from "../../hooks/UseCart";
+import UseAuth from "../../hooks/UseAuth";
+import UseAxiosSecure from "../../hooks/UseAxiosSucure";
+import Swal from "sweetalert2";
 // import CategoryModal from "./categoryModal";
 
 
@@ -13,6 +17,60 @@ import { FaEmpire } from "react-icons/fa6";
 const CategoryDetails = () => {
     const data=useLoaderData();
     console.log(data);
+    const [,refetch]=UseCart()
+    const {user}=UseAuth()
+    const navigate=useNavigate()
+    const axiosSecure=UseAxiosSecure()
+
+    const handleShopingCart = async (item) => {
+        const a = {
+          name: item?.name,
+          medicineId:item._id,
+          description: item?.description,
+          company: item?.company,
+          category: item?.category,
+          discount: item?.discount,
+          price: item?.priceperunit,
+          unit: item?.unit,
+          sellerEmail: item?.email,
+          generic: item?.generic,
+          photo:item?.photo,
+          buyerEmal: user?.email || "",
+         
+        };
+       if (!user || !user?.email) {
+         
+        navigate("/login")   
+       }
+     else{
+      axiosSecure.post("/carts", a)
+      .then(res=>{
+      if (res.data.insertedId) {
+        Swal.fire({
+          title: "Product added sucessfully",
+          showClass: {
+            popup: `
+              animate__animated
+              animate__fadeInUp
+              animate__faster
+            `
+          },
+          hideClass: {
+            popup: `
+              animate__animated
+              animate__fadeOutDown
+              animate__faster
+            `
+          }
+        });
+      }
+      refetch()
+      
+      })
+     
+     }
+        
+      };
     
     return (
         <div className="font-popins font-bold my-20">
@@ -54,14 +112,14 @@ const CategoryDetails = () => {
                   <td className="uppercase ">{item.company}</td>
                   <th>
                     <button
-                      // onClick={() => handleShopingCart(item)}
+                      onClick={() => handleShopingCart(item)}
                       className="btn btn-ghost btn-lg bg-primary"
                     >
                       <FaShoppingCart className="md:text-2xl text-white"></FaShoppingCart>
                     </button>
                   </th>
                   <th>
-                    {/* <Modalad item={item}></Modalad> */}
+                  
                     {item.category}
                   </th>
                   <th>
